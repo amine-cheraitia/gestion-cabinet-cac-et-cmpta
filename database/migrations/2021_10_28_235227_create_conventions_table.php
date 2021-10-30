2902759785e05e017f558cf1e70fdf06850276a7
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-10-30 15:41:05 +0200

    migration & model 100%

--- database/migrations/2021_10_28_235227_create_conventions_table.php
+++ database/migrations/2021_10_28_235227_create_conventions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateConventionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('conventions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('prenom');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('mission_id')->nullable();
+            $table->foreign('mission_id')->references('id')->on('missions');
+            $table->date('date_convention');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('conventions');
     }
 }
