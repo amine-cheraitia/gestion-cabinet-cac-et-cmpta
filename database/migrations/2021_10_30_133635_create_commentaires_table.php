f8158912d65dc6e2ce71123e9ac9a741c8bdf232
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-10-31 16:42:11 +0100

    user role foreign key

--- database/migrations/2021_10_30_133635_create_commentaires_table.php
+++ database/migrations/2021_10_30_133635_create_commentaires_table.php
@@ -23,6 +23,10 @@ class CreateCommentairesTable extends Migration
             $table->foreign('tache_id')->references('id')->on('taches');
             $table->timestamps();
         });
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
     }
 
     /**
