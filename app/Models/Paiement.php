20fe72e5848376b5e7f1f6f70f0889f8f2b01925
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-11-17 12:24:33 +0100

    Relation model paiement

--- app/Models/Paiement.php
+++ app/Models/Paiement.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Paiement extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+
+    public function facture()
+    {
+        return $this->belongsTo(Facture::class, 'facture_id');
+    }
+
+    public function typepaiement()
+    {
+        return $this->belongsTo(TypePaiement::class, 'type_paiement_id');
+    }
 }
