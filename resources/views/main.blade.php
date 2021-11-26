6743de39afd9ebe78338fadddff01d920d4f92dd
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-11-26 21:18:56 +0100

    -

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -315,11 +315,11 @@
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Gestion de Cabinet Cac 2021</div>
+                        <div class="text-muted">Copyright &copy; Gestion de Cabinet Meddahi 2021</div>
                         <div>
                             <a href="#">Privacy Policy</a>
                             &middot;
-                            <a href="#">Terms &amp; Conditions</a>
+                            <a href="#">Terms &amp; Conditions </a>
                         </div>
                     </div>
                 </div>
