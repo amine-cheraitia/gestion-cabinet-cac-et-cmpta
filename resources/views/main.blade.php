d3ccbfbd1f37611599394173fc54b663bcc7e807
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-12-21 09:55:21 +0100

    app.js fixed calendar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
     <script src="{{asset('/js/datatables-simple-demo.js')}}"></script>
     {{-- //laravel mix --}}
-
+    <script src="{{asset('/js/app.js')}}"></script>
     <link href="{{asset('/css/app.css')}}" rel="stylesheet" />
     {{--
     <link rel="stylesheet" href="css/app.css" /> --}}
@@ -361,7 +361,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
-    <script src="{{asset('/js/app.js')}}"></script>
+
     <script src="{{asset('/js/scripts.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     {{-- <script src="{{asset('/assets/demo/chart-area-demo.js')}}"></script>
