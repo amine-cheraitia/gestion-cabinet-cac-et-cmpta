339de8b2846d7d2ce681781aa5f70f919631a1bc
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-12-21 15:06:35 +0100

    add modal to confirm delete mission

--- resources/views/missions/missionList.blade.php
+++ resources/views/missions/missionList.blade.php
@@ -90,8 +90,8 @@ Liste des missions
                                 style="font-size: 15px" class="fas fa-sign-in-alt"></i></a> &nbsp;
                         <a href="{{route('mission.edit',$mission->id)}}" class="btn btn-outline-secondary">
                             <i class="fas fa-minus"></i></a> &nbsp;
-                        <a href="{{route('mission.destroy',$mission->id)}}" class="btn btn-outline-danger"><i
-                                style="font-size: 20px" class="fas fa-times"></i></a>
+                        <a id="{{$mission->id}}" class="btn btn-outline-danger dlt"><i style="font-size: 20px"
+                                class="fas fa-times"></i></a>
 
                     </td>
                 </tr>
@@ -101,5 +101,52 @@ Liste des missions
         </table>
     </div>
 </div>
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editModalLabel">Suppression de mission</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 
+            </div>
+            <div class="modal-body">
+                <p>Etes vous sur de vouloir Supprimer cette mission ?</p>
+            </div>
+            <div class="modal-footer">
+                <a id="confirm" href="" class="btn btn-dark">Oui</a>
+
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+
+        $(".dlt").click(function (e) {
+            e.preventDefault();
+            var url= "{{url('missions/')}}"
+            var id=$(this).attr('id');
+            $("#confirm").attr('href',url+'/'+id+'/delete')
+            $('#editModal').modal('show')
+
+        });
+
+        $("#datatablesSimple").bind("DOMSubtreeModified", function() {
+            $(".dlt").click(function (e) {
+            e.preventDefault();
+            var url= "{{url('missions/')}}"
+            var id=$(this).attr('id');
+            $("#confirm").attr('href',url+'/'+id+'/delete')
+            $('#editModal').modal('show')
+
+            });
+        });
+
+        /*  */
+
+    })
+</script>
 @endsection
