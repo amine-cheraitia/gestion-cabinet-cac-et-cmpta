02e9225cfd407762e372306f998e8648c7d4e416
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-12-24 15:10:07 +0100

    redirection to /

--- routes/web.php
+++ routes/web.php
@@ -143,4 +143,4 @@ Route::prefix('kpi')->name('kpi.')->group(function () {
 //auth
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); */
